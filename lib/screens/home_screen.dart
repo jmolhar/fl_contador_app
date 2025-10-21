d10211fe385efcc42297a5d3e5612064e0efbbf1
jmolhar <rxd54891@gmail.com>
2025-10-21 11:03:25 +0200

    widget independiente sumar restar y reiniciar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,25 +1,88 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int contador = 0;
+
+  void incrementar() {
+    contador++;
+    setState(() {});
+  }
+
+  void reset() {
+    contador = 0;
+    setState(() {});
+  }
+
+  void decrementar() {
+    contador--;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
+    const tamanoLetra = TextStyle(fontSize: 40);
+
     return Scaffold(
       backgroundColor: Colors.deepOrange,
-      appBar: AppBar(title: Text('HomeScreen'), elevation: 50.0),
+      appBar: AppBar(title: Text('Contador'), elevation: 10.0),
       body: Center(
-        child: const Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Instalación'),
-            SizedBox(width: 50.0),
-            Text('Flutter'),
-            Text('y'),
-            Text('Git'),
+            Text('Numero de clics', style: tamanoLetra),
+            SizedBox(height: 20.0),
+            Text('$contador', style: tamanoLetra),
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: CustomizedFloatingActionButton(
+        incrementarFn: incrementar,
+        resetFn: reset,
+        decrementarFn: decrementar,
+      ),
+    );
+  }
+}
+
+class CustomizedFloatingActionButton extends StatelessWidget {
+  final Function incrementarFn;
+  final Function resetFn;
+  final Function decrementarFn;
+
+  const CustomizedFloatingActionButton({
+    super.key,
+    required this.incrementarFn,
+    required this.resetFn,
+    required this.decrementarFn,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        FloatingActionButton(
+          child: const Icon(Icons.remove),
+          //child: const Text('Sumar'),
+          onPressed: () => decrementarFn(),
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.restore),
+          onPressed: () => resetFn(),
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () => incrementarFn(),
+        ),
+      ],
     );
   }
 }
